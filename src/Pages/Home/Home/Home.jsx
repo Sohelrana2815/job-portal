ceb16d775e486221cf37c8d860481ee5e24ce52b
Sohelrana2815 <sohel152302@gmail.com>
2024-11-27 07:48:21 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <>
+      <div className="text-green-600 text-2xl text-center font-medium">
+          Hello World!
+      </div>
+        </>
+    );
+};
+
+export default Home;
